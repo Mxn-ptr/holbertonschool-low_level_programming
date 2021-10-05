e23dc5ceb54746fafce7d64dfa7853b7a1a42456
Mxn-ptr <maxence0@hotmail.fr>
2021-10-05 14:30:44 +0200

    Add task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples 3 or 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, r, i;
+
+	for (i = 0, i < 1024, i++)
+	{
+		if ((i % 3) == 0)
+		{
+			a = a + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			b = b + i;
+		}
+	}
+	r = a + b;
+	printf("%d\n", i);
+	return (0);
+}
