57e976423c273264db667925df543c463427e54c
Mxn-ptr <maxence0@hotmail.fr>
2021-10-05 13:09:37 +0200

    Add addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a : integer
+ * @b : integer
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
