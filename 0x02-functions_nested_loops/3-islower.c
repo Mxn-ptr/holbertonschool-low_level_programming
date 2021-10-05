593fbd78a1b3c8fa1ad59672f49d37a88e4db6f3
Mxn-ptr <maxence0@hotmail.fr>
2021-10-05 10:50:25 +0200

    Add islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * Return: 1 if the character is lowercase, else 0
+ */
+
+int _islower(int c)
+{
+	return(c >= 'a' && c <= 'z');
+}
+
+	
