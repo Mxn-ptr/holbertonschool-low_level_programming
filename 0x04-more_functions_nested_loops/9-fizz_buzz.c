53cabda0eeb33df8d0617080e425639a272495f9
Mxn-ptr <maxence0@hotmail.fr>
2021-10-07 11:06:20 +0200

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Flizz buzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 1)
+			printf("%d", i);
+		else if (i % 3 == 0 && i % 5 != 0)
+			printf(" Fizz");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf(" Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf(" FlizzBuzz");
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
+	return (0);
+}
