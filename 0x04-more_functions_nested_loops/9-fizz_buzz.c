e88cba3669e6e48f1bbed64e5adbbf32cd0a4a55
Mxn-ptr <maxence0@hotmail.fr>
2021-10-07 13:29:42 +0200

    Fix task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,26 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i == 1)
-			printf("%d", i);
-		else if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 != 0)
+		{
 			printf(" Fizz");
+		}
 		else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf(" Buzz");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf(" FlizzBuzz");
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
 		else
+		{
 			printf(" %d", i);
+		}
 	}
 	printf("\n");
 	return (0);
