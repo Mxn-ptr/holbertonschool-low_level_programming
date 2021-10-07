cc7910833556cd3a0277c0b3e45470dd0739b05a
Mxn-ptr <maxence0@hotmail.fr>
2021-10-07 10:00:01 +0200

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9
+ * except 2 and 4
+ * followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if ( i == 50 || i == 52)
+			i++;
+		else
+		{
+			_putchar(i);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
