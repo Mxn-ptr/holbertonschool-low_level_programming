a3261e0e4d94b069c5a3814502e74fe023de714f
Mxn-ptr <maxence0@hotmail.fr>
2021-10-11 10:56:14 +0200

    Add task 0 to 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
