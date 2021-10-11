2b385c99bde637f2dab933d5031e3752c3c2d39a
Mxn-ptr <maxence0@hotmail.fr>
2021-10-11 10:52:14 +0200

    Add task 0 to 7

---
+++ 0x05-pointer_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to check
+ */
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+
+	k = j;
+	for (i = 0; i <= j; i++)
+	{
+		_putchar(str[k++]);
+	}
+	_putchar('\n');
+}
