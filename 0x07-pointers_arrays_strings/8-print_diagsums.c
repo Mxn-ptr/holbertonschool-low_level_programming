a025c7fbb9efca825a3f543329c8c5b17fd2c42a
Mxn-ptr <maxence0@hotmail.fr>
2021-10-18 12:03:43 +0200

    Fix betty task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -22,7 +22,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = size - 1; i >= 0; i--)
 	{
-		sum2 += a[(size *(i + 1)) - (i + 1)];
+		sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
