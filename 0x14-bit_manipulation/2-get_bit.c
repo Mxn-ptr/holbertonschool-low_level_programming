4fa7357dff858139102f19d10d2fa423744b08eb
Mxn-ptr <maxence0@hotmail.fr>
2021-11-18 12:13:22 +0100

    Fix betty style task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,8 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
-	
-	if (index > sizeof(unsigned long int)* 8)
+
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
 	bit = ((n >> index) & 1);
