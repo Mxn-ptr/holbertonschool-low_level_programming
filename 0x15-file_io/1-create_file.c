0f23380f7ee22c831860c6860331e274e0cb8ccb
Mxn-ptr <maxence0@hotmail.fr>
2021-11-22 16:18:08 +0100

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, letters);
 
-	if (o < 0 || w < 0)
+	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
