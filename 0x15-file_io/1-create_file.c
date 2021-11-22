e004db7868717822b6970a1c2b60c21d5f711c16
Mxn-ptr <maxence0@hotmail.fr>
2021-11-22 16:19:26 +0100

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (letters = 0; text_content[letters]; letters++)
-			;
+		for (letters = 0; text_content[letters])
+			letters++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
