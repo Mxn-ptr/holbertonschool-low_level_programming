804e97314a08a3b5540c6ab111114065c187cdf5
Mxn-ptr <maxence0@hotmail.fr>
2022-01-06 12:57:49 +0100

    Fix betty on task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - add the element of all the node of a list
+ * @head: pointer to the head of the list
+ * Return: the sum of the element
+ */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int res = 0;
