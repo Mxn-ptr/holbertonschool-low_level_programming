479bcb6697a028e69b64f90bb7b0296b6afeea8f
Mxn-ptr <maxence0@hotmail.fr>
2022-01-28 15:08:28 +0100

    Fix task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx, i;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
