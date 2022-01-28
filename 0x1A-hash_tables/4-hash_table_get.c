2505e803bfeb8d6e1a7b6cbc34b0bfd8900902aa
Mxn-ptr <maxence0@hotmail.fr>
2022-01-28 15:10:11 +0100

    Fix task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,6 +16,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
+	if (idx > ht->size)
+		return (NULL);
 
 	for (i = idx; ht->array[i]; i++)
 	{
