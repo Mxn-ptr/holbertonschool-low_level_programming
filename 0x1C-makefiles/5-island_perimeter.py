1e740748ba58b74c9430b1fd81a0f8dd8fe8f435
Mxn-ptr <maxence0@hotmail.fr>
2022-03-17 12:58:19 +0100

    Fix pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,9 +3,9 @@
 
 
 def island_perimeter(grid):
-    """ Returns the perimeter of an island: 
-    0 is water, 
-    1 is dirt 
+    """ Returns the perimeter of an island:
+    0 is water,
+    1 is dirt
     """
     width = len(grid[0])
     height = len(grid)
@@ -14,12 +14,12 @@ def island_perimeter(grid):
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                if  grid[i - 1][j] == 0:
+                if grid[i - 1][j] == 0:
                     perimeter = perimeter + 1
-                if  grid[i + 1][j] == 0:
+                if grid[i + 1][j] == 0:
                     perimeter = perimeter + 1
-                if  grid[i][j - 1] == 0:
+                if grid[i][j - 1] == 0:
                     perimeter = perimeter + 1
-                if  grid[i][j + 1] == 0:
+                if grid[i][j + 1] == 0:
                     perimeter = perimeter + 1
     return (perimeter)
