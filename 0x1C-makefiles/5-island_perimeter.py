7b4f027cdd45d49042d1b8517d765c7f6f728470
Mxn-ptr <maxence0@hotmail.fr>
2022-03-17 17:41:22 +0100

    Try to fix task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,7 +5,7 @@
 def island_perimeter(grid):
     """ Returns the perimeter of an island:
     0 is water,
-    1 is dirt
+    1 is land
     """
     width = len(grid[0])
     height = len(grid)
@@ -15,11 +15,11 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 if grid[i - 1][j] == 0:
-                    perimeter = perimeter + 1
+                    perimeter += 1
                 if grid[i + 1][j] == 0:
-                    perimeter = perimeter + 1
+                    perimeter += 1
                 if grid[i][j - 1] == 0:
-                    perimeter = perimeter + 1
+                    perimeter += 1
                 if grid[i][j + 1] == 0:
-                    perimeter = perimeter + 1
+                    perimeter += 1
     return (perimeter)
